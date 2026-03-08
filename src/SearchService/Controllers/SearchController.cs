eaf033869d058088199976b488b902ed7dceec04
doodjo <dusan6006@gmail.com>
2026-03-08 23:26:44 +0100

    sta_znam_8_Mart

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,20 +9,37 @@ namespace SearchService;
 public class SearchController: ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
     {
-        var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item, Item>();
 
-        query.Sort(x => x.Ascending(a => a.Make));
-
-        if (!string.IsNullOrEmpty(searchTerm))
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();    
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();    
         }
+
+        //ostali atributi koje mozemo da koristimo/da ih manipulisemo
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 
-        return result;
+
+
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+
+        });
         
     }
 }
